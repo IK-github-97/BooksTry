704c6f35b188554ab3b07ca5bc12b308a76d3fb0
2212045 <2212045@LTIN408998>
2024-07-26 00:39:06 +0530

    Add project files.

---
+++ Data/BooksTryContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BooksTry.Models;
+
+namespace BooksTry.Data
+{
+    public class BooksTryContext : DbContext
+    {
+        public BooksTryContext (DbContextOptions<BooksTryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BooksTry.Models.Books> Books { get; set; } = default!;
+    }
+}
